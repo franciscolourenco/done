dead2eb24d2335e69ea2bb5aa498675bf6658ec7
Francisco Lourenco <aristidesfl@gmail.com>
2017-02-02 01:21:11 +0100

    Add support for icons and all terminals on macOS
    For icon support you need to install terminal-notifier
    with `brew install terminal-notifier`.

--- _done.fish
+++ _done.fish
@@ -1,3 +1,24 @@
+set _bundleID ''
+
+function _get_bundle_id
+	lsappinfo info -only bundleID (lsappinfo front) | cut -d '"' -f4
+end
+
+function _is_terminal_focused
+	test $_bundleID = _get_bundle_id
+end
+
+function _is_command_available
+	type -q $argv
+end
+
+
+function _started --on-event fish_preexec
+	if _is_command_available lsappinfo
+		set _bundleID _get_bundle_id
+	end
+end
+
 function _done --on-event fish_prompt
 
 	if test $CMD_DURATION
@@ -7,26 +28,29 @@ function _done --on-event fish_prompt
 		# when a command takes longer than notify_duration
 		set notify_duration 10000
 		set exclude_cmd "zsh|bash|less|man|more|ssh|drush php|git add|vim"
+
 		if begin
 				test $CMD_DURATION -gt $notify_duration
 				and echo $history[1] | grep -vqE "^($exclude_cmd).*"
 			end
 
-			switch (uname)
-				case Darwin # macOS
-					echo "
-						tell application \"System Events\"
-							set activeApp to name of first application process whose frontmost is true
-							if \"iTerm\" is not in activeApp then
-								display notification \"Finished in $duration\" with title \"$history[1]\"
-							end if
-						end tell
-						" | osascript
-				case Linux
-					notify-send "Finished in $duration with title $history[1]"
-        case "*"
-          # bell sound
-          echo -e "\a"
+			set -l message "Finished in $duration"
+			set -l title "$history[1]"
+
+			if test (uname) = 'Darwin' # macOS
+				if not _is_terminal_focused
+					if _is_command_available terminal-notifier # if command available
+						terminal-notifier -message "$message" -title "$title" -sender "$_bundleID"
+					else
+						osascript -e "display notification \"$message\" with title \"$title\""
+					end
+				end
+
+			else if _is_command_available notify-send # linux
+				notify-send "Finished in $duration with title $history[1]"
+
+			else # anything else
+      	echo -e "\a" # bell sound
 			end
 		end
 	end
