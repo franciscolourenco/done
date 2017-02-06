25218a7903642fdafacec16c97eaedcafb73e74b
Francisco Lourenco <aristidesfl@gmail.com>
2017-02-06 11:26:46 +0100

    Add window detection to Linux
    Closes #4

--- _done.fish
+++ _done.fish
@@ -23,26 +23,27 @@
 # done v0.4.0
 
 
+set _initial_window_id ''
 
-set _bundleID ''
+function _get_window_id
+	if _is_available lsappinfo
+		lsappinfo info -only bundleID (lsappinfo front) | cut -d '"' -f4
+	else if _is_available xprop
+		xprop -id (xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) _NET_WM_NAME | cut -d "\"" -f 2
+	end
 
-function _get_bundle_id
-	lsappinfo info -only bundleID (lsappinfo front) | cut -d '"' -f4
 end
 
 function _is_terminal_focused
-	test $_bundleID = _get_bundle_id
+	test $_initial_window_id = (_get_window_id)
 end
 
-function _is_command_available
+function _is_available
 	type -q $argv
 end
 
-
 function _started --on-event fish_preexec
-	if _is_command_available lsappinfo
-		set _bundleID _get_bundle_id
-	end
+	set _initial_window_id (_get_window_id)
 end
 
 function _done --on-event fish_prompt
@@ -50,34 +51,31 @@ function _done --on-event fish_prompt
 	if test $CMD_DURATION
 		# Store duration of last command
 		set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
-
-		# when a command takes longer than notify_duration
 		set notify_duration 10000
 		set exclude_cmd "zsh|bash|less|man|more|ssh|drush php|git add|vim"
 
 		if begin
-				test $CMD_DURATION -gt $notify_duration
-				and echo $history[1] | grep -vqE "^($exclude_cmd).*"
+				test $CMD_DURATION -gt $notify_duration  # longer than notify_duration
+				and echo $history[1] | grep -vqE "^($exclude_cmd).*" # not excluded command
+				and not _is_terminal_focused  # terminal or window not in foreground
 			end
 
 			set -l title "Finished in $duration"
 			set -l message "$history[1]"
 
-			if test (uname) = 'Darwin' # macOS
-				if not _is_terminal_focused
-					if _is_command_available terminal-notifier # if command available
-						terminal-notifier -message "$message" -title "$title" -sender "$_bundleID"
-					else
-						osascript -e "display notification \"$message\" with title \"$title\""
-					end
-				end
+			if _is_available terminal-notifier  # https://github.com/julienXX/terminal-notifier
+				terminal-notifier -message "$message" -title "$title" -sender "$_initial_window_id"
 
-			else if _is_command_available notify-send # linux
+			else if _is_available osascript  # AppleScript
+				osascript -e "display notification \"$message\" with title \"$title\""
+
+			else if _is_available notify-send # Linux notify-send
 				notify-send --icon=terminal --app-name=terminal "$title" "$message"
 
-			else # anything else
+			else  # anything else
       	echo -e "\a" # bell sound
 			end
+
 		end
 	end
 end
