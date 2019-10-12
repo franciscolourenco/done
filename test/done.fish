5ccec1900f3c48c16ae7839b7d76269e9d49cd64
Jason <jason@kayoway.com>
2019-10-12 09:35:05 +0900

    Update test file to use latest fishtape syntax

--- test/done.fish
+++ test/done.fish
@@ -1,8 +1,10 @@
+# run this file with jorgebucaran/fishtape
+# i.e. fishtape test/done.fish
+
 function setup
   source conf.d/done.fish
 end
 
-test "command are executed without errors"
-  0 -eq (echo 1 > /dev/null; echo $status)
-end
-
+@test "commands are executed without errors" (
+  echo 1 > /dev/null
+) $status -eq 0
