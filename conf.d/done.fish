019af594f2c03fb37d0bdc846fd9109062b4ae12
Francisco Lourenco <aristidesfl@gmail.com>
2020-04-04 19:53:49 +0200

    1.14.4

--- conf.d/done.fish
+++ conf.d/done.fish
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-set -g __done_version 1.14.3
+set -g __done_version 1.14.4
 
 function __done_get_focused_window_id
     if type -q lsappinfo
