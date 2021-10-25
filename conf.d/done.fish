3b36014b014d5f464ab2a4543ee79918cf69f15a
João Marques <joao.marques.contacto@gmail.com>
2021-10-25 09:47:54 +0100

    Prevent inspecting a process that doesn't exist
    Sometimes, the `__done_is_tmux_window_active` function will try to access the root process (`PID 1`). It is happening to me using `byobu` (`tmux`). 
    When that happens(`tmux_fish_pid=1`), `ps -o ppid= -p $tmux_fish_pid | string trim` will return `0`, causing a failure when we access `basename (ps -o command= -p $tmux_fish_ppid)`
    To ensure we don't get ugly errors if that is the case, we can check it we reached that case before checking the process' `basename`

--- conf.d/done.fish
+++ conf.d/done.fish
@@ -116,6 +116,7 @@ function __done_is_tmux_window_active
     # ppid == "tmux" -> break
     set tmux_fish_pid $fish_pid
     while set tmux_fish_ppid (ps -o ppid= -p $tmux_fish_pid | string trim)
+        and test $tmux_fish_ppid != "0"
         and ! string match -q "tmux*" (basename (ps -o command= -p $tmux_fish_ppid))
         set tmux_fish_pid $tmux_fish_ppid
     end
