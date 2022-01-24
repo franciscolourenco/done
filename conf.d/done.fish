d76cf67260c330f1df8b0b85cd3ee0be5701cfcb
Yuta Taniguchi <yuttie@users.noreply.github.com>
2022-01-24 20:32:01 +0900

    Fix failure of tmux detection
    Obtaining process name with `basename (ps -o command= -p $tmux_fish_ppid)`
    could fail.
    
    Let's suppose tmux was launched as `tmux new-session -c /path/to/dir`.
    The ps command would return `tmux new-session -c /path/to/dir`, and the result
    of taking basename of it would be `dir`, which is not what we expect.
    
    In my opinion, the ps command should return a file path because basename
    command is used.  Prior to the commit 346adbe63714d859aa7fbcfe1fb799de34ddf16c,
    ps was given `-o exe=` instead of `-o command=`.  With `-o exe=`, ps returns a
    path to the tmux executable, and we don't need to care about what kinds of
    arguments were passed to tmux.

--- conf.d/done.fish
+++ conf.d/done.fish
@@ -116,7 +116,7 @@ function __done_is_tmux_window_active
     # ppid == "tmux" -> break
     set tmux_fish_pid $fish_pid
     while set tmux_fish_ppid (ps -o ppid= -p $tmux_fish_pid | string trim)
-        and ! string match -q "tmux*" (basename (ps -o command= -p $tmux_fish_ppid))
+        and ! string match -q "tmux*" (basename (ps -o exe= -p $tmux_fish_ppid))
         set tmux_fish_pid $tmux_fish_ppid
     end
 
