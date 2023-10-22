9e42d1dd2cdd65eec2e29b240895a43f774c530e
Francisco Lourenço <208149+franciscolourenco@users.noreply.github.com>
2023-10-22 12:43:23 +0200

    1.18.0

--- conf.d/done.fish
+++ conf.d/done.fish
@@ -24,7 +24,7 @@ if not status is-interactive
     exit
 end
 
-set -g __done_version 1.17.2
+set -g __done_version 1.18.0
 
 function __done_run_powershell_script
     set -l powershell_exe (command --search "powershell.exe")
